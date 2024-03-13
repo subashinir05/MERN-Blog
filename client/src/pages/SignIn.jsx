bcff0d42e62719373c5d449063ac2a877ab13e82
subashinir05 <subashinir05@gmail.com>
2024-03-13 14:25:06 +0530

    Add redux toolkit

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
-import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
-import React, { useState } from 'react'
-import { Link,useNavigate } from 'react-router-dom'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react';
+import { Link,useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { signInStart, signInSuccess, signInFailure } from '../redux/user/userSlice';
 
 export default function SignIn() {
   const [formData, setFormData] = useState({});
   // signup loading(progess)
-  const [errorMessage, setErrorMessage]=useState(null);
-  const [loading, setLoading]=useState(false);
+  const {loading, error: errorMessage}=useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate=useNavigate();
   const handleChange = (e) => {
     setFormData({...formData, [e.target.id]: e.target.value.trim() });
@@ -15,11 +17,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
       e.preventDefault(); 
       if( !formData.email || !formData.password){
-        return setErrorMessage('Please fill all the fields');
+        return dispatch(signInFailure('Please fill all the fields'));
       }
       try{
-        setLoading(true);
-        setErrorMessage(null);
+        dispatch(signInStart());
         const res = await fetch('/api/auth/signin',{
           method: 'POST',
           headers: {'Content-Type': 'application/json' },
@@ -27,16 +28,15 @@ export default function SignIn() {
         });
         const data=await res.json();
         if(data.success === false){
-          return setErrorMessage(data.message);
+          dispatch(signInFailure(data.message));
         }
-        setLoading(false);
         // navigate to signin page
         if(res.ok){
+          dispatch(signInSuccess(data));
           navigate('/');
         }
       }catch (error){
-        setErrorMessage(error.message);
-        setLoading(false);
+        dispatch(signInFailure(error.message));
       }
   };
   return (
@@ -82,7 +82,7 @@ export default function SignIn() {
           <div className='flex gap-2 text-sm mt-5'>
               <span>Don't have an account?</span>
               <Link to='/sign-up' className='text-blue-400'>
-                Sign Up
+                Sign Up 
               </Link>
           </div>
           {
