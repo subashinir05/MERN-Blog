7e7f23313935cb74f2648f94a7fda9f6bd0a6fe9
subashinir05 <subashinir05@gmail.com>
2024-03-16 14:08:00 +0530

    Add signout functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -5,7 +5,7 @@ import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/st
 import { app } from '../firebase'
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
-import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice';
+import { updateStart, updateSuccess, updateFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure, signoutSuccess } from '../redux/user/userSlice';
 import {useDispatch} from 'react-redux'; 
 import {HiOutlineExclamationCircle} from 'react-icons/hi';
 
@@ -121,6 +121,21 @@ export default function DashProfile() {
         dispatch(deleteUserFailure(error.message));
       }
     };
+    const handleSignout = async () =>{
+      try {
+        const res = await fetch('/api/user/signout', {
+          method: 'POST',
+        });
+        const data = await res.json();
+        if (!res.ok) {
+          console.log(data.message);
+        } else {
+          dispatch(signoutSuccess());
+        }
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-2xl'>Profile</h1>
@@ -162,7 +177,7 @@ export default function DashProfile() {
       </form>
       <div className='text-blue-400 flex justify-between mt-5'>
         <span onClick={() => setShowmodal(true)} className='cursor-pointer'>Delete Account</span>
-        <span className='cursor-pointer'>Sign Out</span>
+        <span onClick={handleSignout} className='cursor-pointer'>Sign Out</span>
       </div>
       {updateUserSuccess && (
         <Alert color='success' className='mt-5'>
