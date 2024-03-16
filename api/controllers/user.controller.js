d76e964c39d0816744a3f46bff7ff47f43390ba8
subashinir05 <subashinir05@gmail.com>
2024-03-16 11:32:10 +0530

    Delete user API route

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -45,3 +45,16 @@ if (req.body.username){
         next(error);        
     }
 };
+
+export const deleteUser = async (req, res, next) => {
+    if(req.user.id !== req.params.userId)
+    {
+        return next(errorHandler(403, 'You are not allowed to delete this user'));
+    }
+    try {
+        await User.findByIdAndDelete(req.params.userId);
+        res.status(200).json('User deleted!!');
+    } catch (error) {
+        next(error);
+    }
+}
