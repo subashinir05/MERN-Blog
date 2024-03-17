dde9e94ae82fc246574b288d93cf042ff0e5c9b7
subashinir05 <subashinir05@gmail.com>
2024-03-17 15:02:20 +0530

    Upload post image functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,9 +1,53 @@
-import { Button, FileInput, Select, TextInput } from 'flowbite-react'
-import React from 'react'
+import { Button, FileInput, Select, TextInput, Alert } from 'flowbite-react'
+import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import {getStorage, uploadBytesResumable, getDownloadURL, ref} from 'firebase/storage';
+import {app} from '../firebase';
+import {CircularProgressbar} from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function CreatePost() {
+    const [file, setFiles] = useState(null)
+    const [imageUploadProgress, setImageUploadProgress] = useState(null)
+    const [imageUploadError, setImageUploadError] = useState(null)
+    const [formData, setFormData] = useState({});
+    const handleUploadImage = async () => {
+        try {
+            if(!file){
+                setImageUploadError('Select an image');
+                return;
+            }
+            setImageUploadError(null)
+            const storage = getStorage(app);
+            const fileName = new Date().getTime() + '-' + file.name;
+            const storageRef = ref(storage, fileName);
+            const uploadTask = uploadBytesResumable(storageRef, file);
+            uploadTask.on(
+                'state_changed',
+                (snapshot) => {
+                  const progress =
+                    (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  setImageUploadProgress(progress.toFixed(0));
+                },
+                (error) => {
+                    setImageUploadError('Image upload failed');
+                    setImageUploadProgress(null);
+                  },
+                  () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                      setImageUploadProgress(null);
+                      setImageUploadError(null);
+                      setFormData({ ...formData, image: downloadURL });
+                    });
+                  }
+                );
+        } catch (error) {
+            setImageUploadError('Image upload failed');
+            setImageUploadProgress(null);
+            console.log(error); 
+        }
+    }
   return (
     <div className='p-3 max-w-3xl mx-auto min-h-screen '>
         <h1 className='text-center text-2xl my-7 font-semibold'>New Post</h1>
@@ -21,11 +65,25 @@ export default function CreatePost() {
                 </Select>
             </div>
             <div className='flex gap-4 items-center justify-between border-2 border-blue-300 border-double p-3 '>
-                <FileInput type='file' accept='image/*'/>
-                <Button type='button'className='bg-gradient-to-r from-blue-400 to-teal-300' size='sm' outline >
-                    Upload Image
+                <FileInput type='file' accept='image/*' onChange={(e) => setFiles(e.target.files[0])}/>
+                <Button type='button'className='bg-gradient-to-r from-blue-400 to-teal-300' size='sm' outline onClick={handleUploadImage} disabled={imageUploadProgress}>
+                {
+                imageUploadProgress ? (
+                <div className='w-16 h-16'>
+                <CircularProgressbar value={imageUploadProgress} text={`${imageUploadProgress || 0}%`}
+                />
+              </div>
+                ) : (
+              'Upload Image'
+                )}
                 </Button>
             </div>
+            { imageUploadError && <Alert color='failure'>{imageUploadError}
+            </Alert>}
+            { formData.image && (
+            <img src={formData.image} alt='upload'className='w-full h-72 object-cover'/>
+            )
+            }
             <ReactQuill theme='snow' placeholder='Add description' className='h-72 mb-12'
             required />
             <Button type='submit'className='bg-gradient-to-r from-teal-300 to-orange-200 text-gray-700'>Add Post</Button>
