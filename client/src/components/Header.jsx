cdc17a0d786b537905ebc3fa058afc015777790c
subashinir05 <subashinir05@gmail.com>
2024-03-22 22:13:40 +0530

    Complete search functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
 import {Avatar, Button, Dropdown, Navbar, TextInput} from 'flowbite-react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 import {AiOutlineSearch} from 'react-icons/ai';
 import {FaMoon, FaSun} from 'react-icons/fa';
 import {useSelector, useDispatch} from 'react-redux';
 import {toggleTheme} from '../redux/theme/themeSlice';
 import { signoutSuccess } from '../redux/user/userSlice';
+import { useEffect, useState } from 'react';
 
 export default function Header() {
     const path=useLocation().pathname;
+    const location = useLocation();
+    const navigate = useNavigate();
     const dispatch = useDispatch();
     const {currentUser} = useSelector(state => state.user)
     const {theme} = useSelector((state) => state.theme)
+    const [searchTerm, setSearchTerm] = useState('')
+
+    useEffect(() => {
+        const urlParams = new URLSearchParams(location.search);
+        const searchTermFromUrl = urlParams.get('searchTerm');
+        if (searchTermFromUrl) {
+          setSearchTerm(searchTermFromUrl);
+        }
+      }, [location.search]);
     const handleSignout = async () =>{
         try {
           const res = await fetch('/api/user/signout', {
@@ -27,16 +39,24 @@ export default function Header() {
           console.log(error.message);
         }
       }
+
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('searchTerm', searchTerm);
+        const searchQuery = urlParams.toString();
+        navigate(`/search?${searchQuery}`);
+      };
+      
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
             <span className='px-2 py-1 bg-gradient-to-r from-purple-400 to-teal-300 rounded-xl'>Suba's</span>
             Blog
         </Link>
-        <form>
+        <form onSubmit={handleSubmit}>
             <TextInput type='text' placeholder='Search..' rightIcon={AiOutlineSearch}
-            className='hidden lg:inline'
-            />
+            className='hidden lg:inline' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
         </form>
         <Button className='w-11 h-10 lg:hidden' color="gray" pill>
             <AiOutlineSearch/>
